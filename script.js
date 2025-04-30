3e85776120cd16142ad4f929e83cae577a8ad534
413X-A <wollekalexander@gmail.com>
2025-04-30 02:54:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -410,6 +410,7 @@ function startProduction() {
 
 // Produktion der Ressourcen
 function produceGold() {
+    checkConnectivity();
     let goldProduced = gridArray.flat().reduce((sum, cell) => {
         if (cell.active && cell.type === "goldmine") {
             const key = `${cell.element.dataset.x}_${cell.element.dataset.y}`;

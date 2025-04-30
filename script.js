7a5d4295baea7ed7df851c77864c1de279c6a60f
413X-A <wollekalexander@gmail.com>
2025-04-30 18:33:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function generateIsland() {
             // leichte Verzerrung + Zufalls-Noise für unregelmäßige Insel
             const noise = (Math.sin(x * 0.1) + Math.cos(y * 0.1)) * 2 + (Math.random() - 0.5) * 2;
 
-            if (distance < islandRadius + noise) {
+            if (distance < islandRadius - 10 + noise) {
                 // Insel
                 gridArray[y][x] = {
                     type: null,
